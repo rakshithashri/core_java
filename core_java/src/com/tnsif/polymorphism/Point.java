133828a8366dbda0d960cbe3ea9a107ec432e789
Rakshitha <rakshithashri2803@gmail.com>
2024-07-04 19:34:56 +0530

    poly commit

---
+++ core_java/src/com/tnsif/polymorphism/Point.java
@@ -0,0 +1,25 @@
+//overloading programs 
+package com.tnsif.polymorphism;
+
+public class Point {
+	// constructor can be overloaded 
+private float x, y;
+	public Point() {
+		x = 0.0f;
+		y = 0.0f;
+		
+		
+	}
+	public Point(float x) {
+		this.x = x;
+	}
+
+	public Point(float x, float y) {
+		this.x=x;
+		this.y=y;
+	}
+	@Override
+	public String toString() {
+		return "Point [x=" + x + ", y=" + y + "]";
+	}
+}
---
+++ core_java/src/com/tnsif/polymorphism/PolyDemo.java
@@ -0,0 +1,17 @@
+package com.tnsif.polymorphism;
+
+public class PolyDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Point p1 = new Point();
+		System.out.println(p1);
+		Point p2 = new Point(2.3f,7.8f);
+		System.out.println(p2);
+		
+		
+
+	}
+
+}
